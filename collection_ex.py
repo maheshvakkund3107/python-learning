262b1088b19127ede434f76ca37c109f634a1a04
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2021-11-29 13:16:17 +0530

    -Set(Collections) and its functions in python.

--- collection_ex.py
+++ collection_ex.py
@@ -65,6 +65,59 @@ def collections(name):
     list_numbers.reverse()
     print(list_numbers)
 
+    # Set
+    s = {1, 2, 3, 4}
+    print(s)
+    empty_set = set()
+    print(empty_set)
+    empty_dict = {}  # this will create a empty dict and not set
+    print(empty_dict)
+    print(2 in s)
+    print(7 in s)
+    print(len(s))
+    print(sorted(s))
+    print(sum(s))
+
+    # Adding and deleting elements in Set
+    set_numbers = {1, 2, 3, 3, 4, 4, 3}
+    set_numbers.add(5)
+    print(set_numbers)
+
+    # update-Updates the set on which update is invoked
+    # update a set with union of itself and others
+    set_numbers.update({4, 5, 6, 7})
+    print(set_numbers)
+
+    # union-Returns the union of sets as a new set
+    set_numbers = {1, 2, 3, 4}
+    union_set = set_numbers.union({7, 8, 9, 0})
+    print(union_set)
+
+    union_set.pop()
+    print(union_set)
+
+    # remove-Removes an element from a set.
+    # Throws error if element does not exist
+    union_set.remove(2)
+    print(union_set)
+
+    # discard -Removes an element from set.
+    # No error if the element does not exist
+    union_set.discard(14)
+    print(union_set)
+
+    union_set.clear()
+    print(union_set)
+
+    # Typical Set Operations
+    s1 = {1, 2, 3, 4}
+    s2 = {3, 4, 5, 6, 7}
+
+    print(s1.union(s2))
+    print(s1.intersection(s2))
+    print(s1.difference(s2))
+    # Validation of set is missing here
+
 
 if __name__ == '__main__':
     collections('PyCharm')
