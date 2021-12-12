a9eb6765b5d301b580ca33163be96442dc763961
maheshvakkund <maheshvakkund3107@outlook.com>
2021-12-12 23:14:31 +0530

    -Compare method to compare ages.

--- constructorsandselfobject.py
+++ constructorsandselfobject.py
@@ -6,13 +6,23 @@ class Computer:
     def update(self):
         self.name = "Abcd"
 
+    def compare(self, other):
+        if self.age == other.age:
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
     comp1 = Computer()
     comp2 = Computer()
     comp1.name = "Sam"
-    comp1.age = 27
+    comp1.age = 25
     print(comp1.name)
     print(comp2.name)
     comp1.update()
     print (comp1.name)
+    if comp1.compare(comp2):
+        print ("They are same Ages")
+    else:
+        print ("They are different Ages")
