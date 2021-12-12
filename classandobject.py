7114e11d92e4024637cbe1d3463b10e2dbe22a98
maheshvakkund <maheshvakkund3107@outlook.com>
2021-12-12 11:55:27 +0530

    -Init Constructor, Self

--- classandobject.py
+++ classandobject.py
@@ -1,15 +1,16 @@
 class Computer:
-    #Everytime method/Function is called using object, init method is called
-    def __init__(self):
-        print ("Inside init method ")
+    # Everytime method/Function is called using object, init method is called
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
 
     def config(self):
-        print ("i5, 10th gen ,1TB")
+        print ("Config is:", self.cpu, self.ram )
 
 
 if __name__ == '__main__':
-    #Object Creation
-    comp1 = Computer()
-    comp2 = Computer()
+    # Object Creation
+    comp1 = Computer("i5", 16)
+    comp2 = Computer("Ryzen", 32)
     comp1.config()
     Computer.config(comp2)
