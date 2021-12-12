c2cd22fba1bdbb1cb0975ad73aafc7e6e7dfff25
maheshvakkund <maheshvakkund3107@outlook.com>
2021-12-12 11:51:28 +0530

    -Comments added

--- classandobject.py
+++ classandobject.py
@@ -1,12 +1,14 @@
 class Computer:
+    #Everytime method/Function is called using object, init method is called
     def __init__(self):
-        pass
+        print ("Inside init method ")
 
     def config(self):
         print ("i5, 10th gen ,1TB")
 
 
 if __name__ == '__main__':
+    #Object Creation
     comp1 = Computer()
     comp2 = Computer()
     comp1.config()
